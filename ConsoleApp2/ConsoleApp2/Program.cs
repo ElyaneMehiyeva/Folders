378c1006bbcbdddb2f435eed7a1766063874bab6
Elyane <elyamehiyeva@gmail.com>
2022-03-11 09:17:24 +0400

    Added task2

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -29,12 +29,31 @@ namespace ConsoleApp2
             Chocolate4.BrandName = "Fabric";
             Chocolate4.Price = 300;
 
-            Console.WriteLine(Chocolate1.GetData());
-            Console.WriteLine(Chocolate2.GetData());
-            Console.WriteLine(Chocolate3.GetData());
-            Console.WriteLine(Chocolate4.GetData());
-            
+            //Console.WriteLine(Chocolate1.GetData());
+            //Console.WriteLine(Chocolate2.GetData());
+            //Console.WriteLine(Chocolate3.GetData());
+            //Console.WriteLine(Chocolate4.GetData());
+            Product[] Chocolates = {Chocolate1,Chocolate2,Chocolate3,Chocolate4};
+            foreach(Product Search in SearchProduct(Chocolates, 400.5, 1500.50))
+            {
+                Console.WriteLine(Search.GetData());
+            }
 
+        }
+        static Product[] SearchProduct(Product[] Products,double minPrice, double maxPrice)
+        {
+            Product[] newProducts = new Product[Products.Length];
+            int index = 0;
+            for (int i = 0; i < Products.Length; i++)
+            {
+                
+                if(Products[i].Price<maxPrice && Products[i].Price>minPrice)
+                {
+                    newProducts[index] = Products[i];
+                    index++;
+                }
+            }
+            return newProducts;
         }
 
 
