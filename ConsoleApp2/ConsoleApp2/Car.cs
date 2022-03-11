b843dc63d25b843f17308e1f2309fadc12d80693
Elyane <elyamehiyeva@gmail.com>
2022-03-11 09:56:55 +0400

    Added Car Class

---
+++ ConsoleApp2/ConsoleApp2/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Car:Vehicle
+    {
+        public string Brand;
+        public string Model;
+        public string GetData()
+        {
+            string data = "Car Name :" + name + " Car Brand : " + Brand + " Car Model :" + Model + " Car Color :" + color;
+            return data;
+        }
+    }
+}
