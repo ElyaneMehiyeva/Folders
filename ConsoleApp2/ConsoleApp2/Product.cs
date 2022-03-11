3875f1834f23717b693eabadc75c87b572ad2f94
Elyane <elyamehiyeva@gmail.com>
2022-03-11 08:48:45 +0400

    Added task 1

---
+++ ConsoleApp2/ConsoleApp2/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Product
+    {
+        public string Name;
+        public string BrandName;
+        public int Price;
+        public string GetData()
+        {
+            string data = $"Product Name : {Name} | Product BrandName {BrandName} | Product Price : {Price}";
+            return data;
+        }
+    }
+
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Product Chocolate1 = new Product();
+            Chocolate1.Name = "Adicto";
+            Chocolate1.BrandName = "Zara";
+            Chocolate1.Price = 1000;
+
+            Product Chocolate2 = new Product
+            {
+                Name = "Snickers",
+                BrandName = "Empty",
+                Price = 500
+            };
+            Product Chocolate3 = new Product
+            {
+                Name = "Alpen Gold",
+                BrandName = "Nest",
+                Price = 2000
+            };
+            Product Chocolate4 = new Product();
+            Chocolate4.Name = "Mars";
+            Chocolate4.BrandName = "Fabric";
+            Chocolate4.Price = 300;
+
+            Console.WriteLine(Chocolate1.GetData());
+            Console.WriteLine(Chocolate2.GetData());
+            Console.WriteLine(Chocolate3.GetData());
+            Console.WriteLine(Chocolate4.GetData());
+            
+
+        }
+
+
+    }
+}
