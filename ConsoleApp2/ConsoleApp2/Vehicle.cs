09ee6ea3d396a39d340cac560c8aa2dd66c8302b
Elyane <elyamehiyeva@gmail.com>
2022-03-11 09:57:17 +0400

    Added Vehicle Class

---
+++ ConsoleApp2/ConsoleApp2/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Vehicle
+    {
+       public string color;
+       public string name;
+
+
+    }
+}
