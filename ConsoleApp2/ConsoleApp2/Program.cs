7bd7441563da0af83d46248ea14faa3b22987f4e
Elyane <elyamehiyeva@gmail.com>
2022-03-11 09:58:08 +0400

    Added task 3

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,7 +6,15 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            
+            /*
+             * 
+             * Vehicle class-i yaradin 
+             * Color ve Name xususiyyetleri olsun.
+             * Car classi yaradin 
+             * Vehicleden miras alsin ve elave olaraq Brand,Model xususiyyetleri olsun
+             * 
+             */
+
             Product Chocolate1 = new Product();
             Chocolate1.Name = "Adicto";
             Chocolate1.BrandName = "Zara";
@@ -33,11 +41,23 @@ namespace ConsoleApp2
             //Console.WriteLine(Chocolate2.GetData());
             //Console.WriteLine(Chocolate3.GetData());
             //Console.WriteLine(Chocolate4.GetData());
-            Product[] Chocolates = {Chocolate1,Chocolate2,Chocolate3,Chocolate4};
-            foreach(Product Search in SearchProduct(Chocolates, 400.5, 1500.50))
+            //Product[] Chocolates = {Chocolate1,Chocolate2,Chocolate3,Chocolate4};
+            //foreach(Product Search in SearchProduct(Chocolates, 400.5, 1500.50))
+            //{
+            //    Console.WriteLine(Search.GetData());
+            //}
+            // Task 3 Calistir
+            Vehicle v1 = new Vehicle
             {
-                Console.WriteLine(Search.GetData());
-            }
+                color = "Red",
+                name = "Name1"
+            };
+            Car car1 = new Car();
+            car1.Brand = "Empty";
+            car1.Model = "En son model";
+            car1.color = "Yellow";
+            car1.name = "Adi";
+            Console.WriteLine(car1.GetData());
 
         }
         static Product[] SearchProduct(Product[] Products,double minPrice, double maxPrice)
